80873734b64fedd22178a0caa415665d175c96cb
Pietro Poluzzi <pietro.poluzzi@dotenv.it>
2021-07-19 17:49:43 +0200

    extract contact info - change message

--- extract_contact_information/extract_contact_information_base_example.py
+++ extract_contact_information/extract_contact_information_base_example.py
@@ -15,11 +15,16 @@ if __name__ == '__main__':
 
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
-    original_message = "..."
+    original_message = """
+    Dear John,
+    I'm really proud of you! I'm writing this email to thank you about yesterday's talk. 
+    If you'd like to collaborate with me, here's my address: 442 Evergreen Terrace, Oregon US 97477
+    Best, Bart
+    """
 
     response = openai.Completion.create(
         engine="davinci-instruct-beta",
-        prompt="Extract the mailing address from this email:\n\nDear Kelly,\n\nIt was great to talk to you at the seminar. I thought Jane's talk was quite good.\n\nThank you for the book. Here's my address 2111 Ash Lane, Crestview CA 92002\n\nBest,\n\nMaya\n\nName and address:\n",
+        prompt="Extract the mailing address from this email:\n\n" + original_message + "\n\nName and address:\n",
         temperature=0,
         max_tokens=64,
         top_p=1.0,
@@ -28,4 +33,4 @@ if __name__ == '__main__':
     )
 
     print("Original message: " + original_message)
-    print("OpenAI response: " + str(response))
+    print("OpenAI response: " + str(response["choices"]))
